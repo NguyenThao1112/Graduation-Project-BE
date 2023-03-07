f191ae77d68911c5a668e0bb251436ccff713b5b
minhduypham <minhduypham@kms-technology.com>
2023-03-08 02:31:56 +0700

    remove some unsuse folder and change structure project

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -3,229 +3,231 @@ const authConstants = require('../constants/messageConstants');
 const validatorHelper = require('../helpers/validatorHelper');
 
 /**
- * 
- * @param {Express.Request} request 
- * @param {Express.Response} response 
- * @returns {Promise}  
+ *
+ * @param {Express.Request} request
+ * @param {Express.Response} response
+ * @returns {Promise}
  */
 function login(request, response) {
-    return new Promise((resolve, reject) => {
-        const {email, password} = request.body;
+  return new Promise((resolve, reject) => {
+    const { email, password } = request.body;
 
-        //Default response is login failed
-        let responseJson = {
-            code: authConstants.AUTH_LOGIN_FAILED_CODE,
-            message: authConstants.AUTH_LOGIN_FAILED_MESSAGE, 
+    //Default response is login failed
+    let responseJson = {
+      code: authConstants.AUTH_LOGIN_FAILED_CODE,
+      message: authConstants.AUTH_LOGIN_FAILED_MESSAGE,
+    };
+
+    //Check if authenticate successfully
+    authService
+      .authenticate(email, password)
+      .then((jwt) => {
+        //If authenticated success => change the response's data
+        if (jwt) {
+          responseJson.code = authConstants.SUCCESSFUL_CODE;
+          responseJson.message = authConstants.AUTH_LOGIN_SUCCESS_MESSAGE;
+          responseJson.token = jwt;
         }
-
-        //Check if authenticate successfully
-        authService.authenticate(email, password)
-            .then((jwt) => {
-
-                //If authenticated success => change the response's data
-                if (jwt) {
-                    responseJson.code = authConstants.SUCCESSFUL_CODE;
-                    responseJson.message = authConstants.AUTH_LOGIN_SUCCESS_MESSAGE;
-                    responseJson.token = jwt;
-                }
-
-            })
-            .catch(error => {
-                console.log(error);
-            })
-            .finally(() => {
-                response.json(responseJson);
-            });
-
-    });
-    
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        response.json(responseJson);
+      });
+  });
 }
 
 /**
- * 
- * @param {Express.Request} request 
- * @param {Express.Response} response 
- * @returns {Promise}  
+ *
+ * @param {Express.Request} request
+ * @param {Express.Response} response
+ * @returns {Promise}
  */
 function signUp(request, response) {
+  return new Promise((resolve, reject) => {
+    //Check if the request is valid
+    const hasError = validatorHelper.verifyValidations(request, response);
+    if (hasError) {
+      return;
+    }
 
-    return new Promise((resolve, reject) => {
+    const { email, password } = request.body;
 
-        //Check if the request is valid
-        const hasError = validatorHelper.verifyValidations(request, response);
-        if (hasError) {
-            return;
-        }
+    //Default response is sign up failed
+    let responseJson = {
+      code: authConstants.AUTH_SIGNUP_FAILED_CODE,
+      message: authConstants.AUTH_SIGNUP_FAILED_MESSAGE,
+    };
 
-        const {email, password} = request.body;
-
-        //Default response is sign up failed
-        let responseJson = {
-            code: authConstants.AUTH_SIGNUP_FAILED_CODE,
-            message: authConstants.AUTH_SIGNUP_FAILED_MESSAGE, 
-        }
-
-        //Check if registrating successfully
-        authService
-            .accountRegistrate(email, password)
-            .then(() => {
-
-                //if then => successfully case
-                responseJson.code = authConstants.SUCCESSFUL_CODE;
-                responseJson.message = authConstants.AUTH_SIGNUP_SUCCESS_MESSAGE;
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-            .finally(() => {
-                response.json(responseJson);
-            });
-    })
+    //Check if registrating successfully
+    authService
+      .accountRegistrate(email, password)
+      .then(() => {
+        //if then => successfully case
+        responseJson.code = authConstants.SUCCESSFUL_CODE;
+        responseJson.message = authConstants.AUTH_SIGNUP_SUCCESS_MESSAGE;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        response.json(responseJson);
+      });
+  });
 }
 
 /**
- * 
- * @param {Express.Request} request 
- * @param {Express.Response} response 
- * @returns {Promise}  
+ *
+ * @param {Express.Request} request
+ * @param {Express.Response} response
+ * @returns {Promise}
  */
- function buildForgetPassword(request, response) {
+function buildForgetPassword(request, response) {
+  return new Promise((resolve, reject) => {
+    //Check if the request is valid
+    const hasError = validatorHelper.verifyValidations(request, response);
+    if (hasError) {
+      return;
+    }
 
-    return new Promise((resolve, reject) => {
+    const { email } = request.body;
 
-        //Check if the request is valid
-        const hasError = validatorHelper.verifyValidations(request, response);
-        if (hasError) {
-            return;
-        }
+    //Default response is failed to send email
+    let responseJson = {
+      code: authConstants.AUTH_FORGET_PASSWORD_FAILED_CODE,
+      message: authConstants.AUTH_FORGET_PASSWORD_FAILED_MESSAGE,
+    };
 
-        const {email} = request.body;
-
-        //Default response is failed to send email
-        let responseJson = {
-            code: authConstants.AUTH_FORGET_PASSWORD_FAILED_CODE,
-            message: authConstants.AUTH_FORGET_PASSWORD_FAILED_MESSAGE,
-        }
-
-        //Check if the email is sent successfully
-        authService.createTokenForForgetPassword(email)
-            .then(() => {
-
-                //if then => successfully case
-                responseJson.code = authConstants.SUCCESSFUL_CODE;
-                responseJson.message = authConstants.AUTH_FORGET_PASSWORD_SUCCESS_MESSAGE;
-                
-            })
-            .catch((error) => {
-                console.log(error);
-            })
-            .finally(() => {
-                response.json(responseJson);
-            });
-        
-    })
+    //Check if the email is sent successfully
+    authService
+      .createTokenForForgetPassword(email)
+      .then(() => {
+        //if then => successfully case
+        responseJson.code = authConstants.SUCCESSFUL_CODE;
+        responseJson.message =
+          authConstants.AUTH_FORGET_PASSWORD_SUCCESS_MESSAGE;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        response.json(responseJson);
+      });
+  });
 }
 
 /**
- * 
- * @param {Express.Request} request 
- * @param {Express.Response} response 
- * @returns {Promise}  
+ *
+ * @param {Express.Request} request
+ * @param {Express.Response} response
+ * @returns {Promise}
  */
 function verifyForgetPasswordToken(request, response) {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    //Check if the request is valid
+    const hasError = validatorHelper.verifyValidations(request, response);
+    if (hasError) {
+      return;
+    }
 
-        //Check if the request is valid
-        const hasError = validatorHelper.verifyValidations(request, response);
-        if (hasError) {
-            return;
+    const token = request.query.token;
+
+    //Default response is sign up failed
+    let responseJson = {
+      code: authConstants.AUTH_FORGET_PASSWORD_VERIFY_INVALID_CODE,
+      message: authConstants.AUTH_FORGET_PASSWORD_VERIFY_INVALID_MESSAGE,
+    };
+
+    //Check if the email is sent successfully
+    authService
+      .verifyForgetPasswordToken(token)
+      .then((errorCode) => {
+        //if then => successfully case
+        responseJson.code = authConstants.SUCCESSFUL_CODE;
+        responseJson.message =
+          authConstants.AUTH_FORGET_PASSWORD_VERIFY_SUCCESS_MESSAGE;
+      })
+      .catch((errorCode) => {
+        responseJson.code = errorCode;
+        if (
+          errorCode === authConstants.AUTH_FORGET_PASSWORD_VERIFY_INVALID_CODE
+        ) {
+          responseJson.message =
+            authConstants.AUTH_FORGET_PASSWORD_VERIFY_INVALID_MESSAGE;
+        } else if (
+          errorCode === authConstants.AUTH_FORGET_PASSWORD_VERIFY_EXPIRE_CODE
+        ) {
+          responseJson.message =
+            authConstants.AUTH_FORGET_PASSWORD_VERIFY_EXPIRE_MESSAGE;
         }
-
-        const token = request.query.token;
-
-        //Default response is sign up failed
-        let responseJson = {
-            code: authConstants.AUTH_FORGET_PASSWORD_VERIFY_INVALID_CODE,
-            message: authConstants.AUTH_FORGET_PASSWORD_VERIFY_INVALID_MESSAGE,
-        }
-
-        //Check if the email is sent successfully
-        authService.verifyForgetPasswordToken(token)
-            .then((errorCode) => {
-                //if then => successfully case
-                responseJson.code = authConstants.SUCCESSFUL_CODE;
-                responseJson.message = authConstants.AUTH_FORGET_PASSWORD_VERIFY_SUCCESS_MESSAGE;
-            })
-            .catch((errorCode) => {
-                responseJson.code = errorCode;
-                if (errorCode === authConstants.AUTH_FORGET_PASSWORD_VERIFY_INVALID_CODE) {
-                    responseJson.message = authConstants.AUTH_FORGET_PASSWORD_VERIFY_INVALID_MESSAGE;
-                } else if (errorCode === authConstants.AUTH_FORGET_PASSWORD_VERIFY_EXPIRE_CODE) {
-                    responseJson.message = authConstants.AUTH_FORGET_PASSWORD_VERIFY_EXPIRE_MESSAGE;
-                }
-
-            })
-            .finally(() => {
-                response.json(responseJson);
-            });
-        
-    })
+      })
+      .finally(() => {
+        response.json(responseJson);
+      });
+  });
 }
 
 /**
- * 
- * @param {Express.Request} request 
- * @param {Express.Response} response 
- * @returns {Promise}  
+ *
+ * @param {Express.Request} request
+ * @param {Express.Response} response
+ * @returns {Promise}
  */
 function changePasswordAfterForgeting(request, response) {
-    
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
+    //Check if the request is valid
+    const hasError = validatorHelper.verifyValidations(request, response);
+    if (hasError) {
+      return;
+    }
 
-        //Check if the request is valid
-        const hasError = validatorHelper.verifyValidations(request, response);
-        if (hasError) {
-            return;
+    const { token, password } = request.body;
+
+    //Default response is sign up failed
+    let responseJson = {
+      code: authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_INVALID_CODE,
+      message:
+        authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_INVALID_MESSAGE,
+    };
+
+    //Check if registrating successfully
+    authService
+      .changePasswordViaToken(token, password)
+      .then((errorCode) => {
+        //if then => successfully case
+        responseJson.code = errorCode;
+        responseJson.message =
+          authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_SUCCESS_MESSAGE;
+      })
+      .catch((errorCode) => {
+        responseJson.code = errorCode;
+        if (
+          errorCode ===
+          authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_INVALID_CODE
+        ) {
+          responseJson.message =
+            authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_INVALID_MESSAGE;
+        } else if (
+          errorCode ===
+          authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_EXPIRE_CODE
+        ) {
+          responseJson.message =
+            authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_EXPIRE_MESSAGE;
+        } else if (errorCode === authConstants.UNEXPECTED_ERROR_CODE) {
+          responseJson.message = authConstants.UNEXPECTED_ERROR_MSG;
         }
-
-        const {token, password} = request.body;
-
-        //Default response is sign up failed
-        let responseJson = {
-            code: authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_INVALID_CODE,
-            message: authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_INVALID_MESSAGE,
-        }
-
-        //Check if registrating successfully
-        authService
-            .changePasswordViaToken(token, password)
-            .then((errorCode) => {
-
-                //if then => successfully case
-                responseJson.code = errorCode;
-                responseJson.message = authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_SUCCESS_MESSAGE;
-            })
-            .catch((errorCode) => {
-                responseJson.code = errorCode;
-                if (errorCode === authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_INVALID_CODE) {
-                    responseJson.message = authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_INVALID_MESSAGE;
-                } else if (errorCode === authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_EXPIRE_CODE) {
-                    responseJson.message = authConstants.AUTH_FORGET_PASSWORD_CHANGE_PASSWORD_EXPIRE_MESSAGE;
-                } else if (errorCode === authConstants.UNEXPECTED_ERROR_CODE) {
-                    responseJson.message = authConstants.UNEXPECTED_ERROR_MSG;
-                }
-
-            })
-            .finally(() => {
-                response.json(responseJson);
-            });
-    })
+      })
+      .finally(() => {
+        response.json(responseJson);
+      });
+  });
 }
 
 module.exports = {
-    login, 
-    signUp,
-    buildForgetPassword,
-    verifyForgetPasswordToken,
-    changePasswordAfterForgeting,
-}
+  login,
+  signUp,
+  buildForgetPassword,
+  verifyForgetPasswordToken,
+  changePasswordAfterForgeting,
+};
