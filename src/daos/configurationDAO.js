14847e0eeda841345cc01a9233c09cc62146dac6
dangkl123 <nhatdang2604@gmail.com>
2023-03-22 14:35:29 +0700

    #Mentor:#Configuration:#Academic-Title: Finish clone the DAO layer for academic title feature, from academic rank feature

--- src/daos/configurationDAO.js
+++ src/daos/configurationDAO.js
@@ -405,6 +405,209 @@ function createAcademicRanks(academicRanks) {
     
 }
  
+
+/****************************************************************
+ ***********************ACADEMIC TITLE*****************************
+ ****************************************************************/
+
+
+/**
+ *  Query to get all the academic title
+ *  with offset and limit size for pagination
+ * 
+ * @param {int} offset
+ * @param {int} limitSize 
+ * @return {Promise}
+ */
+ function getAcademicTitleWithPagination(offset, limitSize) {
+    return new Promise(function (resolve, reject) {
+        const query = 
+        [
+            `SELECT id, name, created_at, updated_at`, 
+            'FROM academic_title',
+            queryConstants.FILTER_DELETED_RECORD_QUERY,
+            `ORDER BY id ASC`,
+            'LIMIT ?, ?',
+        ].join(' ');
+
+        let academicTitles = null;
+        connection.query(query, [offset, limitSize], (error, results, fields) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            academicTitles = results;
+            resolve(academicTitles);
+        });
+    })
+    
+}
+
+/**
+ *  Query to get all the academic title, but with only its id and name
+ * 
+ * @return {Promise}
+ */
+function getAllAcademicTitle() {
+    return new Promise(function (resolve, reject) {
+        const query = 
+        [
+            `SELECT id, name`, 
+            'FROM academic_title',
+            queryConstants.FILTER_DELETED_RECORD_QUERY,
+            `ORDER BY id ASC`,
+        ].join(' ');
+
+        let academicTitles = null;
+        connection.query(query, (error, results, fields) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            academicTitles = results;
+            resolve(academicTitles);
+        });
+    })
+    
+}
+
+
+/**
+ *  Query to get a academic title by its id
+ * @param {number} id
+ * @return {Promise}
+ */
+ function getAcademicTitleById(id) {
+    return new Promise(function (resolve, reject) {
+        const query = 
+        [
+            `SELECT id, name`, 
+            'FROM academic_title',
+            `WHERE id = ?`,
+            'LIMIT 1'
+        ].join(' ');
+
+        let academicTitle = null;
+        connection.query(query, [id], (error, results, fields) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            academicTitle = results;
+            resolve(academicTitle);
+        });
+    })
+    
+}
+
+/**
+ *  Query to update a academic title
+ * @param {Object{id: number, name: string}} academicTitle
+ * @return {Promise}
+ */
+ function updateAcademicTitle(academicTitle) {
+    const {id, name} = academicTitle;
+
+    return new Promise(function (resolve, reject) {
+        const query = 
+        [
+            'UPDATE academic_title',
+			'SET name = ?, updated_at = ?',
+			'WHERE id = ?',
+        ].join(' ');
+
+        const now = moment().utc().format('YYYY/MM/DD hh:mm:ss');
+
+		connection.query(
+			query,
+			[name, now, id],
+			function (error, results, fields) {
+				if (error) {
+					reject(error);
+					return;
+				}
+
+				resolve(academicTitle);
+			}
+		);
+    })
+    
+}
+
+
+/**
+ *  Query to delete multiple academic titles at the same time with the given ids
+ * 
+ * @param {Array<number>} ids
+ * @return {Promise}
+ */
+ function deleteAcademicTitles(ids) {
+    return new Promise(function (resolve, reject) {
+
+        //Using in to avoid n + 1 problem
+        const query = 
+        [
+            'UPDATE academic_title',
+			'SET is_deleted = ?, updated_at = ?',
+			'WHERE id IN (?)',
+        ].join(' ');
+
+        const now = moment().utc().format('YYYY/MM/DD hh:mm:ss');
+
+        connection.query(query, [true, now, ids], (error, result) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            //Number of records are deleted
+            const size = result.affectedRows;
+            resolve(size);
+        });
+    })
+    
+}
+
+/**
+ *  Query to create multiple academic titles at the same time
+ * 
+ * @param {Array<Object{name: string}>} academicTitles
+ * @return {Promise}
+ */
+function createAcademicTitles(academicTitles) {
+    return new Promise(function (resolve, reject) {
+        const query = 
+        [
+            `INSERT INTO academic_title (name, created_at, updated_at, is_deleted)`, 
+            'VALUES ?',
+        ].join(' ');
+
+        const now = moment().utc().format('YYYY/MM/DD hh:mm:ss');
+        const is_deleted = false;
+        const values = academicTitles.map(academicTitle => [academicTitle.name, now, now, is_deleted]);
+
+        //Using bulk insertion for better performance
+        connection.query(query, [values], (error, result) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            //Get the id of the created academic titles
+            const size = result.affectedRows;
+            const firstId = result.insertId;
+            const aboveMaxId = firstId + size;
+            let ids = [];
+            for (let i = firstId; i < aboveMaxId; i++) {
+                ids.push(i);
+            }
+
+            resolve(ids);
+        });
+    })
+    
+}
+
 module.exports = {
 
     //Contact types
@@ -422,4 +625,12 @@ module.exports = {
     getAcademicRankById,
     updateAcademicRank,
     deleteAcademicRanks,
+
+    //Academic titles
+    getAcademicTitleWithPagination,
+    getAllAcademicTitle,
+    createAcademicTitles,
+    getAcademicTitleById,
+    updateAcademicTitle,
+    deleteAcademicTitles,
 }
