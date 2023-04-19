c4da1fa4c2c6b681c413707a4a46c4ec59ca7c19
minhduypham <minhduypham@kms-technology.com>
2023-04-19 08:58:49 +0700

    add new table script

--- scripts/database.sql
+++ scripts/database.sql
@@ -44,6 +44,47 @@ CREATE TABLE lecturer_information (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE phd_thesis (
+    id INT NOT NULL AUTO_INCREMENT,
+    lecturer_id INT NOT NULL,
+    project_name VARCHAR(255) DEFAULT NULL,
+    phd_name VARCHAR(255) DEFAULT NULL,
+    graduate_year INT DEFAULT NULL,
+    note VARCHAR(255) DEFAULT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE book (
+    id INT NOT NULL AUTO_INCREMENT,
+    lecturer_id INT NOT NULL,
+    name VARCHAR(255) DEFAULT NULL,
+    project_id INT NOT NULL,
+    publisher_name VARCHAR(255) DEFAULT NULL,
+    public_year INT NOT NULL,
+    co_authors VARCHAR(255) DEFAULT NULL,
+    pseudonym VARCHAR(255) DEFAULT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE book_author(
+    id INT NOT NULL AUTO_INCREMENT,
+    lecturer_id int NOT NULL,
+    book_id int NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY(id)
+)
+
 CREATE TABLE contact_type (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) DEFAULT NULL,
@@ -56,11 +97,23 @@ CREATE TABLE contact_type (
     PRIMARY KEY(id)
 );
 
+CREATE TABLE contact (
+    id INT NOT NULL AUTO_INCREMENT,
+    lecturer_id INT NOT NULL,
+    contact_type_id INT NOT NULL,
+    value VARCHAR(255) NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,  
+
+    PRIMARY KEY(id)
+)
+
 CREATE TABLE academic_rank (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) DEFAULT NULL,
 
-     -- metadata stuffs
+    -- metadata stuffs
     created_at DATETIME,
     updated_at DATETIME,
     is_deleted BOOLEAN DEFAULT FALSE,  
@@ -215,3 +268,169 @@ CREATE TABLE author(
     
     PRIMARY KEY(id)  
 );
+
+CREATE TABLE project (
+    id INT NOT NULL AUTO_INCREMENT,
+    lecturer_id INT NOT NULL,
+    project_code VARCHAR(255) DEFAULT NULL,
+    from_date DATETIME DEFAULT NULL,
+    to_date DATETIME DEFAULT NULL,
+    expenditure INT NOT NULL,
+    project_role VARCHAR(255) DEFAULT NULL,
+    acceptance_date DATETIME,
+    result VARCHAR(255) DEFAULT NULL,
+    organization VARCHAR(255) DEFAULT NULL,
+    note VARCHAR(255) DEFAULT NULL,
+    reference VARCHAR(255) DEFAULT NULL,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE university (
+    id INT NOT NULL AUTO_INCREMENT,
+    university_name VARCHAR(255) DEFAULT NULL,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY(id)
+
+)
+
+CREATE TABLE current_discipline (
+    id INT NOT NULL AUTO_INCREMENT,
+    lecturer_id INT NOT NULL,
+    discipline_id INT NOT NULL,
+    department_id INT NOT NULL,
+    university_id INT NOT NULL,
+    position VARCHAR(255) DEFAULT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE discipline (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) DEFAULT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) DEFAULT NULL,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE expertise (
+    id INT NOT NULL AUTO_INCREMENT,
+    lecturer_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    specialization VARCHAR(255) NOT NULL,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE research_field (
+    id INT NOT NULL AUTO_INCREMENT,
+    lecturer_id INT NOT NULL,
+    research_name VARCHAR(255) NOT NULL,
+    note VARCHAR(255) NOT NULL,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE degree (
+    id INT NOT NULL AUTO_INCREMENT,
+    discipline_id INT NOT NULL,
+    lecturer_id INT NOT NULL,
+    academic_title_id INT NOT NULL,
+    university_id INT NOT NULL,
+    graduation_thesis_name  VARCHAR(255) DEFAULT NULL,
+    graduation_date DATETIME DEFAULT NULL,
+    
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE work_position (
+    id INT NOT NULL AUTO_INCREMENT,
+    lecturer_id INT NOT NULL,
+    university_id INT NOT NULL,
+    company VARCHAR(255) DEFAULT NULL,
+    position VARCHAR(255) DEFAULT NULL,
+    is_now BOOLEAN DEFAULT FALSE,
+    from_date DATETIME DEFAULT NULL,
+    to_date DATETIME DEFAULT NULL,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE university_office (
+    id INT NOT NULL AUTO_INCREMENT,
+    university_id INT NOT NULL,
+    address VARCHAR(255) DEFAULT NULL,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE activity (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) DEFAULT NULL,
+    type_id INT NOT NULL,
+    lecturer_id INT NOT NULL,
+    content VARCHAR(255) DEFAULT NULL,
+    from_date DATETIME,
+    to_date DATETIME,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE activity_type (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) DEFAULT NULL,
+
+    created_at DATETIME,
+    updated_at DATETIME,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    
+    PRIMARY KEY(id)
+)
