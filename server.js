9f4069c1dbcb2caf53ef76ac814809e1bb45c479
minhduypham <minhduypham@kms-technology.com>
2023-05-04 22:40:11 +0700

    add port 8080

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ const rootUrl = urls.ROOT_API_URL;
 app.use(logger);
 
 // Cross Origin Resource Sharing
-const whitelist = ['http://localhost:3001', 'http://localhost:5000'];
+const whitelist = [
+	'http://localhost:3001',
+	'http://localhost:5000',
+	'http://localhost:8080',
+];
 const corsOptions = {
 	origin: (origin, callback) => {
 		if (whitelist.indexOf(origin) !== -1 || !origin) {
